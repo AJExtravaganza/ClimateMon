c5218c7024e4349db0e57870539c79cdc2a0502b
Alex Dunn <alexdunnmobile@gmail.com>
2017-12-10 21:16:21 -0800

    Should probably commit before getting too deep into the datalogging.

---
+++ datalogger.cpp
@@ -0,0 +1,6 @@
+#include "datalogger.h"
+
+Datalogger::Datalogger()
+{
+
+}
---
+++ datalogger.h
@@ -0,0 +1,11 @@
+#ifndef DATALOGGER_H
+#define DATALOGGER_H
+
+
+class Datalogger
+{
+public:
+    Datalogger();
+};
+
+#endif // DATALOGGER_H
